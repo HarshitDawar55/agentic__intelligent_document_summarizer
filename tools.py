06fed2dfb6773760c371d10691c058835b97da35
HarshitDawar55 <harshit.dawar55@gmail.com>
2025-01-06 04:02:14 +0530

    Intelligent Doc Summarizer using AWS berdock complete application

--- tools.py
+++ tools.py
@@ -15,19 +15,19 @@ S3_BUCKET = os.getenv("S3_BUCKET")
 
 # Definning the required AWS Clients
 s3_client = boto3.client(
-    "s3",
+    service_name="s3",
     region_name="us-east-1",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
 )
 textract_client = boto3.client(
-    "textract",
+    service_name="textract",
     region_name="us-east-1",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
 )
 bedrock_client = boto3.client(
-    "bedrock-runtime",
+    service_name="bedrock-runtime",
     region_name="us-east-1",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
